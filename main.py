e473c4b0e8a9cd37e1141e81773c607b65c5b7e3
itiaz05 <itiaz05@gmail.com>
2021-08-29 18:37:44 +0300

    moving files

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-
-
-
+import os
+import shutil
 
 print("enter source folder's adress:")
 srcFold = input()
 print("enter destination folder's adress:")
 dstFold = input()
+
+shutil.move(srcFold, dstFold)
