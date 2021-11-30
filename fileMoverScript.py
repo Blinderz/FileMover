e997df44dd6bc94868572867be71601d656c6aed
Itai <itiaz05@gmail.com>
2021-11-30 20:58:42 +0200

    Script generalization

--- fileMoverScript.py
+++ fileMoverScript.py
@@ -3,16 +3,16 @@ from os import listdir
 from os.path import isfile, join
 import re
 
-#declarations
+
 #replace the * with your pc username
-dstFold = "C:/Users/itiaz/Downloads"
-mypath = "C:/Users/itiaz/Downloads/"
-srcFold = "C:/Users/itiaz/Desktop"
+dstFold = "C:/Users/*/Downloads"
+mypath = "C:/Users/*/Downloads/"
+srcFold = "C:/Users/*/Desktop"
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]  # take all the file name in array
 stopCond = len(onlyfiles)
 if(stopCond>0):   #there are files in the folder
     for x in onlyfiles:
        nameArr = re.split(',| . |_|-|!|\+', x)
        if (len(nameArr)>=2):
-            if (nameArr[0] == "one" or nameArr[1] == "One"):    #replace 'Nanatsu' with your search keywords
+            if (nameArr[0] == "one" or nameArr[1] == "Nanatsu"):    #replace 'Nanatsu' & 'One' with your search keywords
                shutil.move(srcFold.format(x), dstFold)
